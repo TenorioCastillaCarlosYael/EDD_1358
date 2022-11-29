e681c83cbba0e4218095bdb09e8521e75c894129
Tenorio Castilla Carlos Yael <carlos.tenorio.14.81.33@gmail.com>
2022-11-29 09:22:22 -0600

    Actualización General de Carpetas

---
+++ Arbol_ADT.java
@@ -0,0 +1,18 @@
+package com.urbenia.arreglos_adt;
+
+/**
+ *
+ * @author yael_
+ * @param <T>
+ */
+public class Arbol_ADT<T> {
+
+    private NodoArbolB raiz;
+    private int Rozmiar;
+
+    public Arbol_ADT() {
+        this.raiz = null;
+        this.Rozmiar = 0;
+    }
+    
+}
